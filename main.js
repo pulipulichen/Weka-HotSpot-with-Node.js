f5f85f67193f109778ac9e606af88c297648ab50
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-12-08 15:30:39 +0800

    1208 開始跟Weka介接

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+fs = require('fs');
+path = require('path');
+const ini = require('node-ini');
+cfg = ini.parseSync('./config.ini');
+shell = require('shelljs');
+sleep = require('sleep');
 
+require('./lib/time-utils.js');
+require('./lib/file-utils.js');
+require('./lib/weka-utils.js');
 
+var _input_files = get_input_files();
+var _output_dir = './output/' + get_date_time();
+mkdir(_output_dir);
+
+//console.log(_input_files);
+//console.log(cfg);
+//console.log(_exclude_files);
+//console.log(_input_files);
+
+//var _weka_cmds = build_weka_hotspot_commands(_input_files, _output_dir);
+//console.log(_weka_cmds);
+//console.log(build_weka_hotspot_command(_input_files[0]));
+run_weka_hotspot_commands(_input_files);
--- output-style/script.js
+++ output-style/script.js
@@ -1,7 +1,53 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(function () {
+    // 為item-display加上屬性
+    $("table").each(function (_i, _table) {
+        _table = $(_table);
+        
+        var _match_count = [];
+        var _match_list = [];
 
+        _table.find('.lhs-item-display').each(function (_i, _ele) {
+            _ele = $(_ele);
+            var _text = _ele.text();
+            
+            var _pos = _text.indexOf(" ");
+            if (_pos === -1) {
+                _pos = _text.indexOf("=");
+            }
+            
+            var _field = _text.substr(0, _pos);
+            
+            _ele.attr('data-item-field', _field);
 
+            if ($.inArray(_field, _match_count) === -1) {
+                _match_count.push(_field);
+            }
+            else if ($.inArray(_field, _match_list) === -1) {
+                _match_list.push(_field);
+            }
+        });
+
+        // -------------------
+        // 檢查第一個類型跟第二個類型有沒有重複的表格
+
+        for (var _i in _match_list) {
+            var _field = _match_list[_i];
+
+            _table.find('.lhs-item-display[data-item-field="' + _field + '"]').addClass('match');
+        }
+
+        _table.find('.lhs-item-display[data-item-field]').click(function () {
+            var _table = $(this).parents("table:first");
+            if ($(this).hasClass("highlight")) {
+                _table.find('.lhs-item-display[data-item-field]').removeClass("highlight");
+                return;
+            }
+            
+            _table.find('.lhs-item-display[data-item-field]').removeClass("highlight");
+
+            var _field = $(this).attr("data-item-field");
+            _table.find('.lhs-item-display[data-item-field="' + _field + '"]').addClass("highlight");
+        });
+    });
+        
+});
