c0fed47a4e5c0dfa07694ba1f36efbf8abd844bc
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-12-08 16:09:11 +0800

    1208 取得target

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ sleep = require('sleep');
 
 require('./lib/time-utils.js');
 require('./lib/file-utils.js');
-require('./lib/weka-utils.js');
+require('./lib/weka-hotspot-utils.js');
+require('./lib/string-utils.js');
+require('./lib/json-utils.js');
 
 var _input_files = get_input_files();
 var _output_dir = './output/' + get_date_time();
@@ -21,4 +23,6 @@ mkdir(_output_dir);
 //var _weka_cmds = build_weka_hotspot_commands(_input_files, _output_dir);
 //console.log(_weka_cmds);
 //console.log(build_weka_hotspot_command(_input_files[0]));
-run_weka_hotspot_commands(_input_files);
+var _hotspot_result_raw = WekaHotSpotUtils.run_commands(_input_files);
+var _hotspot_result = WekaHotSpotUtils.parsing_raw_result(_hotspot_result_raw);
+//console.log(_weka_result_raw);
