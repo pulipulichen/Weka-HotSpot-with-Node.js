f33b2623866fbbc9cb9de5fe30ce404b7d5ac33f
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-12-09 09:02:31 +0800

    1209 來做統計的部分，目標是先把資料整理好

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ require('./lib/file-utils.js');
 require('./lib/weka-hotspot-utils.js');
 require('./lib/string-utils.js');
 require('./lib/json-utils.js');
+require('./lib/csv-utils.js');
 
 var _input_files = get_input_files();
 var _output_dir = './output/' + get_date_time();
@@ -15,10 +16,16 @@ var _output_dir = './output/' + get_date_time();
 //console.log(cfg);
 //console.log(_exclude_files);
 //console.log(_input_files);
-
+/*
 //var _weka_cmds = build_weka_hotspot_commands(_input_files, _output_dir);
 //console.log(_weka_cmds);
 //console.log(build_weka_hotspot_command(_input_files[0]));
 var _hotspot_result_raw = WekaHotSpotUtils.run_commands(_input_files);
 var _hotspot_result = WekaHotSpotUtils.parsing_raw_result(_hotspot_result_raw);
 //console.log(_weka_result_raw);
+*/
+
+var _csv1 = CSVUtils.read(_input_files[0]);
+var _csv2 = CSVUtils.target_attribute_filter(_csv1);
+console.log(_csv2);
+
--- require-packages.js
+++ require-packages.js
@@ -4,3 +4,6 @@ path = require('path');
 ini = require('node-ini');
 shell = require('shelljs');
 sleep = require('sleep');
+
+// npm install csv
+csv_parse = require('csv-parse/lib/sync');
