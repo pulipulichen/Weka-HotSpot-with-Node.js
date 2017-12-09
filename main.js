e7582d86a68c8ef947f984802ca8b2e6b0ec8aef
Pulipuli Chen <pulipuli.chen@gmail.com>
2017-12-09 22:52:01 +0800

    1209 範本建立到一半

--- main.js
+++ main.js
@@ -22,10 +22,20 @@ var Main = {
         this.setup_cache();
         this.stat();
         this.hotspot();
+        this.setup_view();
         //this.add_stat_to_hotspot();
         
-        TmpUtils.append(this.data.stat);
-        TmpUtils.append(this.data.hotspot);
+        //TmpUtils.append(this.data.stat);
+        //TmpUtils.append(this.data.hotspot);
+        //TmpUtils.append(this.data.view);
+        //var _full_report = TemplateUtils.render("full-report", { view: this.data.view });
+        //TmpUtils.append(_full_report);
+        
+        this.render();
+        TmpUtils.append(this.data.render);
+        
+        console.log("======================");
+        console.log("Finish!");
     },
     data: {},
     setup_data: function () {
@@ -45,25 +55,49 @@ var Main = {
     },
     stat: function () {
         var _cache_key = this.data.cache_key + "_stat";
+        var _cache_key_t = this.data.cache_key + "_target";
         this.data.stat = cache.get(_cache_key);
+        this.data.target_attribute_options = cache.get(_cache_key_t);
         if (this.data.stat !== null) {
             return;
         }
         
-        var _csv1 = CSVUtils.read(this.data.input_files[0]);
-        var _group_json = CSVUtils.group_by_target_attribute(_csv1);
+        var _stat_data = {};
+        for (var _i = 0; _i < this.data.input_files.length; _i++) {
+            var _file_path = this.data.input_files[_i];
+            var _file_name = path.basename(_file_path, '.csv');
+            var _csv1 = CSVUtils.read(_file_path);
+            var _group_json = CSVUtils.group_by_target_attribute(_csv1);
+            
+            //Wconsole.log(_group_json);
+            var _target_attribute_options = [];
+            for (var _attr in _group_json) {
+                for (var _option in _group_json[_attr]) {
+                    _target_attribute_options.push(_option);
+                }
+                break;
+            }
+            //this.data.target_attribute_options[_file_name] = _target_attribute_options;
 
-        var _flat_json = CSVUtils.flat_attribute_data(_group_json);
-        var _types_json = CSVUtils.detect_attribute_type(_flat_json);
-        this.data.attributes_type = _types_json;
+            var _flat_json = CSVUtils.flat_attribute_data(_group_json);
+            var _types_json = CSVUtils.detect_attribute_type(_flat_json);
+            //this.data.attributes_type[_file_name] = _types_json;
 
-        //console.log(_types_json);
-        var _group_json = CSVUtils.count_nominal_attribute_data(_group_json, _types_json);
-        var _group_json = CSVUtils.stat.analyse(_group_json, _types_json);
-        this.data.stat = _group_json;
-        cache.set(_cache_key, _group_json);
-        //console.log(_group_json);
+            //console.log(_types_json);
+            var _group_json = CSVUtils.count_nominal_attribute_data(_group_json, _types_json);
+            var _group_json = CSVUtils.stat.analyse(_group_json, _types_json);
+            
+            _stat_data[_file_name] = {
+                target_attribute_options: _target_attribute_options,
+                attributes_type: _types_json,
+                attributes: _group_json
+            };
+        }
+        this.data.stat = _stat_data;
         
+        cache.set(_cache_key, _stat_data);
+        cache.set(_cache_key_t, this.data.target_attribute_options);
+        //console.log(_group_json);
     },
     hotspot: function () {
         var _cache_key = this.data.cache_key + "_hotspot";
@@ -84,18 +118,18 @@ var Main = {
         
         var _stat = this.data.stat;
         var _hotspot = this.data.hotspot;
-        var _types = this.data.attributes_type;
+        //var _types = this.data.attributes_type;
         for (var _file_name in _hotspot) {
             for (var _direction in _hotspot[_file_name]) {
                 for (var _r = 0; _r < _hotspot[_file_name][_direction].length; _r++) {
                     var _rules = _hotspot[_file_name][_direction][_r];
                     var _group = _rules.rhs.value;
-
+                    var _types = _stat[_file_name]["attributes_type"];
                     for (var _l = 0; _l < _rules.lhs.length; _l++) {
                         var _attr = _rules.lhs[_l].attribute;
                         var _value = _rules.lhs[_l].value;
+                        var _attr_stat = _stat[_file_name]["attributes"][_attr];
                         var _type = _types[_attr];
-                        var _attr_stat = _stat[_attr];
 
                         var _test, _comparison;
 
@@ -114,13 +148,45 @@ var Main = {
                 }   // for (var _r = 0; _r < this.data.hotspot[_file_name][_direction].length; _r++) {
             }   // for (var _direction in this.data.hotspot[_file_name]) {
         }   // for (var _file_name in this.data.hotspot) {
+    },
+    setup_view: function () {
+        var _cache_key = this.data.cache_key + "_view";
+        this.data.view = cache.get(_cache_key);
+        if (this.data.view !== null) {
+            return;
+        }
         
+        var _view = {};
+        for (var _file_name in this.data.stat) {
+            _view[_file_name] = {
+                stat: this.data.stat[_file_name],
+                hotspot: {
+                    max: this.data.hotspot[_file_name]["max"],
+                    min: this.data.hotspot[_file_name]["min"]
+                }
+            }
+        }
+        this.data.view = _view;
         
+        cache.set(_cache_key, _view);
+    },
+    render: function () {
+        var _render = this.data.view
+        for (var _file_name in _render) {
+            var _r = {};
+            var _stat_data = _render[_file_name]["stat"];
+            _r["stat_table"] = TemplateUtils.render_stat_table(_stat_data);
+            var _max_data = _render[_file_name]["hotspot"]["max"];
+            var _min_data = _render[_file_name]["hotspot"]["min"];
+            _render[_file_name] = _r;
+        }
+        
+        this.data.render = _render;
     }
 };
 
 TmpUtils.remove();
-//Main.exec();
+Main.exec();
 
 //cache.init({'ttl':10000});
 /*
@@ -143,8 +209,10 @@ var output = Mustache.render("{{ title }} spends {{ calc }}", view);
 console.log(output);
 */
 
+/*
 var _data = ChiSquareUtils.example_data;
 var _result = TemplateUtils.render("test", {ct: _data});
 //TmpUtils.append(TemplateUtils.convert_to_template_json(_data));
 console.log(_result);
 TmpUtils.append(_result);
+*/
--- utils/template-utils.js
+++ utils/template-utils.js
@@ -28,10 +28,43 @@ TemplateUtils = {
         }
     },
     render: function (_template_name, _data) {
-        _data = this.convert_to_template_json(_data);
+        //for (var _i in _data) {
+        //    _data[_i] = this.convert_to_template_json(_data[_i]);
+        //}
+        //TmpUtils.append(_data);
         var _template_path = "./templates/" + _template_name + ".html";
         var _template = fs.readFileSync(_template_path,'utf8');
-        console.log(_template);
+        //console.log(_template);
         return Mustache.render(_template, _data);
-    }
+    },
+    render_stat_table: function (_stat_data) {
+        //console.log(_target_attribute_options);
+        var _thead = this.render("stat-table-thead", _stat_data);
+        
+        var _tbody_tr_array = [];
+        for (var _attr in _stat_data["attributes"]) {
+            var _type = _stat_data["attributes_type"][_attr];
+            var _attr_data = _stat_data["attributes"][_attr];
+            
+            if (_type === "numeric") {
+                _tbody_tr_array.push(this.render_stat_tr_numeric(_attr_data));
+            }
+            else if (_type === "nominal") {
+                _tbody_tr_array.push(this.render_stat_tr_nominal(_attr_data));
+            }
+        }
+        
+        var _tbody = this.render("stat-table-tbody", {tr: _tbody_tr_array});
+        
+        return {
+            thead: _thead,
+            tbody: _tbody
+        };
+    },
+    render_stat_tr_numeric: function (_attr_data) {
+        return "<tr>numeric</tr>";
+    },
+    render_stat_tr_nominal: function (_attr_data) {
+        return "<tr>nominal</tr>";
+    },
 };
